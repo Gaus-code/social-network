0f71d8e7d1b19f04c69f428e71c640146f846e45
Yulya Gaus <pesosssss@yandex.ru>
2023-10-06 09:49:04 +0300

    adding NavLink that helps to not reload page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
             <Route path='/profile' Component={Profile} />
           </Routes>
         </div>
-        <Dialogs />
       </div>
     </BrowserRouter>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import classes from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return <nav className={classes.navbar}>
         <ul className={classes.navbar__list}>
             <li className={classes.navbar__item}>
-                <a href="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://www.pinclipart.com/picdir/big/413-4132058_spring-home-icon-blue-png-clipart.png" alt="home-icon" />Home</a>
+                <NavLink to="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://www.pinclipart.com/picdir/big/413-4132058_spring-home-icon-blue-png-clipart.png" alt="home-icon" />Home</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="/profile" className={classes.navbar__link}><img className={classes.home_icon} src="https://www.iconpacks.net/icons/2/free-briefcase-icon-1965-thumb.png" alt="about us" /> profile</a>
+                <NavLink to="/profile" className={classes.navbar__link}><img className={classes.home_icon} src="https://www.iconpacks.net/icons/2/free-briefcase-icon-1965-thumb.png" alt="about us" /> profile</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/5968/5968517.png" alt="docs" /> docs</a>
+                <NavLink to="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/5968/5968517.png" alt="docs" /> docs</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/189/189249.png" alt="download" /> download</a>
+                <NavLink to="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/189/189249.png" alt="download" /> download</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/2057/2057748.png" alt="support" /> support</a>
+                <NavLink to="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/2057/2057748.png" alt="support" /> support</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/1899/1899917.png" alt="menu" /> menu</a>
+                <NavLink to="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://cdn-icons-png.flaticon.com/512/1899/1899917.png" alt="menu" /> menu</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Settings-icon.png" alt="settings" /> settings</a>
+                <NavLink to="#" className={classes.navbar__link}><img className={classes.home_icon} src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Settings-icon.png" alt="settings" /> settings</NavLink>
             </li>
             <li className={classes.navbar__item}>
-                <a href="/dialogs" className={classes.navbar__link}><img className={classes.home_icon} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Google_Contacts_icon_%282022%29.svg/423px-Google_Contacts_icon_%282022%29.svg.png?20221103112314" alt="contacts" /> dialogs</a>
+                <NavLink to="/dialogs" className={classes.navbar__link}><img className={classes.home_icon} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Google_Contacts_icon_%282022%29.svg/423px-Google_Contacts_icon_%282022%29.svg.png?20221103112314" alt="contacts" /> dialogs</NavLink>
             </li>
         </ul>
     </nav>
